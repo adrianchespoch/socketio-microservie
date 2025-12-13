4c6f4bccbe35b167e3c5301aa9b3ffaf982fb7f0
root <root@seupre.ultralink.ec>
2025-12-13 15:50:27 -0500

    no

--- src/shared/infrastructure/config/envs.ts
+++ src/shared/infrastructure/config/envs.ts
@@ -7,7 +7,7 @@ export const envs = {
   PORT: get('PORT').required().asPortNumber(),
 
   // MongoDB
-  MONGODB_URI: get('MONGODB_URI').required().asString(),
-  MONGODB_NAME: get('MONGODB_NAME').required().asString(),
+  // MONGODB_URI: get('MONGODB_URI').required().asString(),
+  // MONGODB_NAME: get('MONGODB_NAME').required().asString(),
 
 };
